92911b9f3513cfbf2800e2bd6aa80435b37ff4fb
Roberto Freitas <robertosouzafreitas@gmail.com>
2024-06-30 11:53:50 -0300

    FuçaoFatorial

--- Fatorial.py
+++ Fatorial.py
@@ -1,5 +1,9 @@
-x = 1
-n = int(input("Digite um numero:"))
-for i in range(1, n +1):
-    x = x*i
-    print(x)
+def fatorial(n):
+    the_product = 1
+    while n >0:
+        the_product *= n 
+        n = n -1
+    print(the_product) 
+
+
+fatorial(5)
