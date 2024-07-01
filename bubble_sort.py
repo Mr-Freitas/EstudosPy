0de69975561c8dd810a977eadc9dd5a3357fb72a
Roberto Freitas <robertosouzafreitas@gmail.com>
2024-07-01 20:43:08 -0300

    Ordenação por inserção

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,13 @@
 def bubbleSort(list):
     listLength = len(list) -1
     for i in range (listLength):
+        no_swaps = False
         for j in range(listLength):
             if list[j] > list[j +1 ]:
                 list[j], list[j+1] = list[j+1],list[j]
-    return list
+                no_swaps = True
+        if no_swaps:    
+            return list
 
 list=[34,6,20,56,1]
 
